e073b6f4f25ba3dc192b006cf7cda16e77d4629d
Amaar Ebrahim <amaaraebrahim@gmail.com>
2021-01-16 14:58:08 -0600

    t

--- Documents/ML/src/K-Armed-Bandits/banditCollection.ts
+++
@@ -1,9 +0,0 @@
-import { Bandit } from "./bandit";
-
-export class BanditCollection {
-    private bandits: Array<Bandit>;
-
-    constructor(banditsIn: Array<Bandit>) {
-        this.bandits = banditsIn;
-    }
-}
